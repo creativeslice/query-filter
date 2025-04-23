c601b9cc9ee524c4f912bf1bbd3e3a8b7d682953
Tim Bowen <tim@creativeslice.com>
2025-04-23 14:01:56 -0700

    feature: option to display filters as buttons

--- src/taxonomy/render.php
+++ src/taxonomy/render.php
@@ -30,13 +30,37 @@ if ( is_wp_error( $terms ) || empty( $terms ) ) {
 ?>
 
 <div <?php echo get_block_wrapper_attributes( [ 'class' => 'wp-block-query-filter' ] ); ?> data-wp-interactive="query-filter" data-wp-context="{}">
-	<label class="wp-block-query-filter-post-type__label wp-block-query-filter__label<?php echo $attributes['showLabel'] ? '' : ' screen-reader-text' ?>" for="<?php echo esc_attr( $id ); ?>">
-		<?php echo esc_html( $attributes['label'] ?? $taxonomy->label ); ?>
-	</label>
-	<select class="wp-block-query-filter-post-type__select wp-block-query-filter__select" id="<?php echo esc_attr( $id ); ?>" data-wp-on--change="actions.navigate">
-		<option value="<?php echo esc_attr( $base_url ) ?>"><?php echo esc_html( $attributes['emptyLabel'] ?: __( 'All', 'query-filter' ) ); ?></option>
-		<?php foreach ( $terms as $term ) : ?>
-			<option value="<?php echo esc_attr( add_query_arg( [ $query_var => $term->slug, $page_var => false ], $base_url ) ) ?>" <?php selected( $term->slug, wp_unslash( $_GET[ $query_var ] ?? '' ) ); ?>><?php echo esc_html( $term->name ); ?></option>
-		<?php endforeach; ?>
-	</select>
+    <?php if ( ! empty( $attributes['label'] ) && $attributes['showLabel'] ) : ?>
+        <label class="wp-block-query-filter-post-type__label wp-block-query-filter__label<?php echo $attributes['showLabel'] ? '' : ' screen-reader-text' ?>" for="<?php echo esc_attr( $id ); ?>">
+            <?php echo esc_html( $attributes['label'] ?? $taxonomy->label ); ?>
+        </label>
+    <?php endif; ?>
+
+    <?php if ( ! empty( $attributes['displayAsButtons'] ) ) : ?>
+        <div class="wp-block-query-filter-taxonomy__buttons">
+			<button type="button"
+				value="<?php echo esc_attr( $base_url ); ?>"
+				data-wp-on--click="actions.navigate"
+				<?php echo empty( $_GET[ $query_var ] ) ? 'aria-current="true"' : ''; ?>
+				class="wp-block-query-filter-taxonomy__button">
+				<?php echo esc_html( $attributes['emptyLabel'] ?: __( 'All', 'query-filter' ) ); ?>
+			</button>
+			<?php foreach ( $terms as $term ) : ?>
+				<button type="button"
+					value="<?php echo esc_attr( add_query_arg( [ $query_var => $term->slug, $page_var => false ], $base_url ) ); ?>"
+					data-wp-on--click="actions.navigate"
+					<?php echo $term->slug === ( $_GET[ $query_var ] ?? '' ) ? 'aria-current="true"' : ''; ?>
+					class="wp-block-query-filter-taxonomy__button">
+					<?php echo esc_html( $term->name ); ?>
+				</button>
+			<?php endforeach; ?>
+		</div>
+    <?php else : ?>
+		<select class="wp-block-query-filter-post-type__select wp-block-query-filter__select" id="<?php echo esc_attr( $id ); ?>" data-wp-on--change="actions.navigate">
+			<option value="<?php echo esc_attr( $base_url ) ?>"><?php echo esc_html( $attributes['emptyLabel'] ?: __( 'All', 'query-filter' ) ); ?></option>
+			<?php foreach ( $terms as $term ) : ?>
+				<option value="<?php echo esc_attr( add_query_arg( [ $query_var => $term->slug, $page_var => false ], $base_url ) ) ?>" <?php selected( $term->slug, wp_unslash( $_GET[ $query_var ] ?? '' ) ); ?>><?php echo esc_html( $term->name ); ?></option>
+			<?php endforeach; ?>
+		</select>
+	<?php endif; ?>
 </div>
