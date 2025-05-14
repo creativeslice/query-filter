74a5a9b48065d50904c44ead7087c5244a0974ee
Tim Bowen <tim@creativeslice.com>
2025-05-13 22:19:58 -0700

    build with buttons

--- build/taxonomy/index.asset.php
+++ build/taxonomy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'cb60058843ed629d1f85');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '9f327c78a8a1b37b9d6c');
