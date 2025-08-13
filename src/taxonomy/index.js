e4dbc57ac6ff4468edeb016281a45ba445609ab2
Tim Bowen <tim@creativeslice.com>
2025-08-13 12:54:23 -0700

    update: convert CSS to SCSS

--- src/taxonomy/index.js
+++ src/taxonomy/index.js
@@ -1,7 +1,7 @@
 import { registerBlockType } from '@wordpress/blocks';
 import Edit from './edit';
 import metadata from './block.json';
-import './style-index.css';
+import './style-index.scss';
 
 registerBlockType( metadata.name, {
 	/**
